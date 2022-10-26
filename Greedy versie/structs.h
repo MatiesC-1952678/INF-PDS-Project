adae2dbc54784d608a6139075db654ba12527bb7
YarneD-1952226 <71635329+YarneD-1952226@users.noreply.github.com>
2022-10-26 20:00:37 +0200

    Added greedy version

---
+++ Greedy versie/structs.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <vector>
+#include <stdio.h>
+
+// TODO@yarne: maybe put this in a seperate file (like my structs.h file)
+struct point
+{
+	std::vector<double> datapoints;
+
+	point() {
+		datapoints = std::vector<double>{};
+	};
+
+	~point(){};
+
+	void addDataPoint(const double inputDataPoint)
+	{
+		datapoints.push_back(inputDataPoint);
+	}
+
+	double getDataPoint(const size_t index)
+	{
+		return datapoints[index];
+	}
+
+	size_t getSize()
+	{
+		return datapoints.size();
+	}
+
+	void add(point &p)
+	{
+		if (datapoints.size() <= 0)
+			datapoints = std::vector<double>(p.getSize(), 0);
+
+		for (size_t i = 0; i < p.getSize(); i++)
+			datapoints[i] += p.getDataPoint(i);
+	}
+
+    void del(point &p)
+    {
+        if (datapoints.size() <= 0)
+			datapoints = std::vector<double>(p.getSize(), 0);
+
+        for (size_t i = 0; i < p.getSize(); i++)
+			datapoints[i] -= p.getDataPoint(i);
+    }
+
+	void divide(const size_t divider)
+	{
+		for (size_t i = 0; i < datapoints.size(); i++)
+			datapoints[i] /= divider;
+	}
+
+    point divideTo( const size_t divider)
+    {
+		point result{};
+        for (size_t i = 0; i < datapoints.size(); i++)
+			result.addDataPoint(datapoints[i] / divider);
+		return result;
+    }
+};
